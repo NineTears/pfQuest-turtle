ea41b6488ffb9a8b217d6563fe597b48557f7932
久永 <jiuyong@hotmail.com>
2023-12-01 23:29:53 +0800

    验证使用修正机制对于部分任务进行了修正的可行性。

---
+++ db/zhCN/overrite-turtle.lua
@@ -0,0 +1,6 @@
+local db = pfDB["quests"]["zhCN-turtle"]
+
+db[41053]["T"] = "The Galak Messenger"
+db[807].T = "Durotar Scouring"
+db[41039].T = "Provisions for Thalanaar"
+db[41040].T = "Woodpaw Disarray"
