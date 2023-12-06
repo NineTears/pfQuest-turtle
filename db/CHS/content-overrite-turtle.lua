cfe4aafd6d252e8469c744f9e276a855a7862861
久永 <jiuyong@hotmail.com>
2023-12-06 23:33:30 +0800

    增加了覆盖数据的机制。

---
+++ db/CHS/content-overrite-turtle.lua
@@ -0,0 +1,23 @@
+local function overrite(src, ovr)
+    for key, value in pairs(ovr) do   -- 遍历原始对象的键值对
+        if type(value) == 'table' then
+            overrite(src[key], value) -- 递归复制键和值到新的表
+        else
+            src[key] = value
+        end
+    end
+end
+
+
+
+-- 任务文本整体修正：
+
+-- local db = {
+--     ["quests"] = {
+--         ["zhCN-turtle"] = {
+
+--         }
+--     }
+-- }
+
+overrite(pfDB, OverriteDB)
