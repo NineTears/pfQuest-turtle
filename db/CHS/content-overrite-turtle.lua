2db00243d2d34b0d43cd77480e63150bc109ad00
久永 <jiuyong@hotmail.com>
2023-12-20 21:13:01 +0800

    为原库中的缺失和结构类型不一致做了额外处理。

--- db/CHS/content-overrite-turtle.lua
+++ db/CHS/content-overrite-turtle.lua
@@ -1,9 +1,15 @@
 local function overrite(src, ovr)
-    for key, value in pairs(ovr) do   -- 遍历原始对象的键值对
-        if type(value) == 'table' then
-            overrite(src[key], value) -- 递归复制键和值到新的表
-        else
+    for key, value in pairs(ovr) do -- 遍历原始对象的键值对
+        local src_v = src[key]
+
+        if nil == src_v then
             src[key] = value
+        else
+            if type(value) == 'table' and type(src_v) == 'table' then
+                overrite(src_v, value) -- 递归复制键和值到新的表
+            else
+                src[key] = value
+            end
         end
     end
 end
